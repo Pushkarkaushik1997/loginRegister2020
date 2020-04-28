a7ea0ae6a9e55265e96b009184a47c912cc7a21d
Pushkarkaushik1997 <pushkarkaushik1997@gmail.com>
2020-04-28 22:14:54 +0530

    Pull

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(express.static(__dirname + '/dist/loginRegister2020'));
 
 app.get('*', function(req,res) {
   // Replace the '/dist/<to_your_project_name>/index.html'
-  res.sendFile(path.join(__dirname+ '/dist/loginRegister2020'));
+  res.sendFile(path.join(__dirname+ '/dist/loginRegister2020/index.html'));
 });
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
